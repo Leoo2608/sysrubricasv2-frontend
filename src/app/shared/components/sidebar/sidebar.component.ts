3ac53fe9ebc053cdcd0dc4a77033d355a7aff969
Leo <leonardo.antazu@upeu.edu.pe>
2020-12-15 19:41:18 -0500

    pyfinalowo

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -46,7 +46,9 @@ export class SidebarComponent implements OnInit {
   isModConf():boolean{
     if(this.router.url==='/moduloconfig' || this.router.url==='/moduloconfig/lineas'
     || this.router.url==='/moduloconfig/planes' || this.router.url==='/moduloconfig/planlineas'
-    || this.router.url==='/moduloconfig/competenciasniveles' || this.router.url==='/moduloconfig/semestres'){
+    || this.router.url==='/moduloconfig/competenciasniveles' || this.router.url==='/moduloconfig/semestres'
+    || this.router.url==='/moduloconfig/competencia' || this.router.url==='/moduloconfig/competenciascursos' ){
+    
       return true;
     }else{
       return false;
